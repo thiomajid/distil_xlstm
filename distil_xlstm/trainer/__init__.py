0a42fd0878ee92f14716b02155797c6641bddfd4
thiomajid <tchiombianomajid@gmail.com>
2024-12-30 18:32:54 +0100

    Add VanillaTrainer class and update distillation_config.yaml for model hub ID

--- distil_xlstm/trainer/__init__.py
+++ distil_xlstm/trainer/__init__.py
@@ -1,4 +1,5 @@
 from .kd_trainer import KDTrainer
 from .trainer_arguments import KDArguments
+from .vanilla_trainer import VanillaTrainer
 
-__all__ = ["KDArguments", "KDTrainer"]
+__all__ = ["KDArguments", "KDTrainer", "VanillaTrainer"]
---
+++ distil_xlstm/trainer/vanilla_trainer.py
@@ -0,0 +1,38 @@
+from functools import partial
+
+import torch.nn.functional as F
+from transformers import AutoTokenizer, Trainer
+from transformers.modeling_outputs import CausalLMOutputWithPast
+
+from distil_xlstm.modeling import DistilxLSTM
+from distil_xlstm.trainer.trainer_arguments import KDArguments
+
+
+class VanillaTrainer(Trainer):
+    def __init__(
+        self,
+        model: DistilxLSTM,
+        args: KDArguments,
+        tokenizer: AutoTokenizer,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            args=args,
+            model=model,
+            processing_class=tokenizer,
+            **kwargs,
+        )
+
+        self.args = args
+        self.kl_loss_fn = partial(F.kl_div, reduction="batchmean")
+
+    def compute_loss(self, model: DistilxLSTM, inputs, return_outputs=False, **kwargs):
+        student_output: CausalLMOutputWithPast = model(
+            **inputs,
+            output_hidden_states=True,
+        )
+
+        if return_outputs:
+            return (student_output.loss, student_output)
+
+        return student_output.loss
