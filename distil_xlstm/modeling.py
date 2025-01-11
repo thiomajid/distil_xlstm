886be771fd7aa62f2d12150f5f15720bd4cf166c
thiomajid <tchiombianomajid@gmail.com>
2025-01-11 01:19:39 +0100

    Update DistilxLSTM model and configuration: add v2 parameter for enhanced model structure and adjust training parameters for improved performance

--- distil_xlstm/modeling.py
+++ distil_xlstm/modeling.py
@@ -106,6 +106,7 @@ class DistilxLSTM(PreTrainedModel):
         tokenizer: AutoTokenizer,
         xlstm_config_path: str,
         return_xlstm_config: bool = False,
+        v2: bool = False,
     ):
         # freezing the teacher
         for param in teacher_model.parameters():
@@ -116,6 +117,19 @@ class DistilxLSTM(PreTrainedModel):
             xlstm_config_dict = yaml.safe_load(file)
             xlstm_config_dict["vocab_size"] = teacher_config.vocab_size
             xlstm_config_dict["embedding_dim"] = teacher_config.hidden_size
+
+        if v2:
+            xlstm_config_dict["num_blocks"] = teacher_config.num_hidden_layers // 2
+            xlstm_config_dict["slstm_at"] = list(
+                range(0, teacher_config.num_hidden_layers, 2)
+            )
+
+            teacher_num_heads = teacher_config.num_attention_heads
+            while teacher_num_heads % 4 == 0:
+                teacher_num_heads += 1
+
+            xlstm_config_dict["mlstm_block"]["mlstm"]["num_heads"] = teacher_num_heads
+            xlstm_config_dict["slstm_block"]["slstm"]["num_heads"] = teacher_num_heads
 
         parsed_config = DistilxLSTMConfig.parse_xlstm_config_dict(
             copy.deepcopy(xlstm_config_dict)
@@ -137,12 +151,15 @@ class DistilxLSTM(PreTrainedModel):
         teacher_model: AutoModelForCausalLM,
         tokenizer: AutoTokenizer,
         xlstm_config_path: str,
+        v2: bool = False,
     ) -> "DistilxLSTM":
+        
         model, config = DistilxLSTM.init_for_distillation(
             teacher_model=teacher_model,
             tokenizer=tokenizer,
             xlstm_config_path=xlstm_config_path,
             return_xlstm_config=True,
+            v2=v2,
         )
 
         model = model.to(teacher_model.device)
