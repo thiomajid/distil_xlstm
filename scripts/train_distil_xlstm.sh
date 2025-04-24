08640b8aa97fc5650ce11ed06019d1937ebcf9e2
thiomajid <tchiombianomajid@gmail.com>
2025-04-24 22:38:22 +0100

    Fix model name in training script: updated model parameter from 'train_distil_xlstm' to 'distil_xlstm'.

--- scripts/train_distil_xlstm.sh
+++ scripts/train_distil_xlstm.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 python3 train_distil_xlstm.py \
-    model=train_distil_xlstm \
+    model=distil_xlstm \
     ++model.num_blocks_init="same" \
     ++trainer.hub_token="${HUB_TOKEN}" \
     ++trainer.hub_model_id="${HUB_MODEL_ID}" \
